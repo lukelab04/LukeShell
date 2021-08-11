3476c576a13cfaa94b96d8c5bc74e2463b24e206
Luke LaBonte <79181426+lukelab04@users.noreply.github.com>
2021-08-11 18:20:58 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -135,7 +135,7 @@ void executeBuiltIns(string builtin, string input = ""){
         file.close();
 
         //Path to the shell executable 
-        string shellpath = "./LukeScript/bin/Debug/net5.0/shell";
+        string shellpath = "./LukeScript/bin/Debug/net5.0/LukeScript";
 
         //Use the shlLaunch function to start lukescript
         char* tempArgs[] = {&shellpath[0], NULL};
